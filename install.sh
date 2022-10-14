11ccd1dcd8256bc30d7734b57e8c8e157d2bf28b
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-10-14 23:18:00 +0300

    noconfirm mode to the installation

--- install.sh
+++ install.sh
@@ -18,9 +18,12 @@ do
     fi
 
   else
-    read -p "Want to link $f (y/n)? " -n 1 -r
-    echo
-    if [[  $REPLY =~ ^[Yy]$ ]]
+    if [[ $1 != --noconfirm ]]
+    then
+      read -p "Want to link $f (y/n)? " -n 1 -r
+      echo
+    fi
+    if [[ $1 == --noconfirm || $REPLY =~ ^[Yy]$ ]]
     then
       ln -s $file ~/.config/$f
       echo -e "${GREEN}linked $f${NC}"
@@ -43,9 +46,12 @@ do
         fi
 
       else
-        read -p "Want to link $f (y/n)? " -n 1 -r
-        echo
-        if [[  $REPLY =~ ^[Yy]$ ]]
+        if [[ $1 != --noconfirm ]]
+        then
+          read -p "Want to link $f (y/n)? " -n 1 -r
+          echo
+        fi
+        if [[ $1 == --noconfirm || $REPLY =~ ^[Yy]$ ]]
         then
           ln -s ~/.dotfiles/$f ~/$f
           echo -e "${GREEN}linked $f${NC}"
@@ -57,9 +63,12 @@ done
 # packer install
 if [[ ! -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]
 then
-  read -p "Install nvim packer (y/n)? " -n 1 -r
-  echo
-  if [[ $RELY =~ ^[Yy] ]]
+  if [[ $1 != --noconfirm ]]
+  then
+    read -p "Install nvim packer (y/n)? " -n 1 -r
+    echo
+  fi
+  if [[ $1 == --noconfirm || $REPLY =~ ^[Yy]$ ]]
   then
     git clone --depth 1 https://github.com/wbthomason/packer.nvim\
      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -82,9 +91,12 @@ do
     fi
 
   else
-    read -p "Want to link $f (y/n)? " -n 1 -r
-    echo
-    if [[  $REPLY =~ ^[Yy]$ ]]
+    if [[ $1 != --noconfirm ]]
+    then
+      read -p "Want to link $f (y/n)? " -n 1 -r
+      echo
+    fi
+    if [[ $1 == --noconfirm || $REPLY =~ ^[Yy]$ ]]
     then
       sudo ln -s $file /usr/local/bin/$f
       echo -e "${GREEN}linked $f${NC}"
